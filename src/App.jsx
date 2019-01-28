8885c39e09c3e69a3b500d9e9cc33268fa55481e
Kristen <kristen.fyffe@gmail.com>
2019-01-28 01:17:57 +0000

    Added components to app.jsx and created ChatBar.jsx, Message.jsx, and MessageList.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import Message from './Message.jsx';
+import MessageList from './MessageList.jsx'
+
+{
+  currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+  messages: [
+    {
+      username: "Bob",
+      content: "Has anyone seen my marbles?",
+    },
+    {
+      username: "Anonymous",
+      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+    }
+  ]
+}
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
-    );
+      <div>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <MessageList />
+        <ChatBar />
+      </div>
+     );
   }
 }
 export default App;
