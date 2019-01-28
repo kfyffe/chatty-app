dbec0103d725beb6f7b2ac05b0cf7f346a325482
Kristen <kristen.fyffe@gmail.com>
2019-01-28 19:45:04 +0000

    added onKeyDown function when user types a message and hits enter. Still working on saving the entered message.

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+
+  handleNewMessage = () => {
+    // let newMessage = event.target.value
+    // this.setState ({
+    //   message: newMessage
+    // })
+  }
+
+  handleKeyPress = (event) => {
+    console.log('Event: ', event.key)
+    if (event.key === 'Enter') {
+      this.handleNewMessage();
+    }
+  }
+
   render(){
     return(
       <footer className="chatbar">
         <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.user.name}/>
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyDown={this.handleKeyPress} />
       </footer>
 
       )
